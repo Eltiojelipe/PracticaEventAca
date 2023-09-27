def690907c1cfc9d1e07afedb6571d322b84fda9
Eltiojelipe <sebadago.2019@gmail.com>
2023-09-27 11:23:49 -0500

    eventos migrations

---
+++ EventosAca/EventosAca.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EventosAca.Share.Entities;
+
+namespace EventosAca.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<EventoAcademico> Eventos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    }
+
+
+
---
+++ EventosAca/EventosAca.Share/Entities/EventoAcademico.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventosAca.Share.Entities
+{
+    public class EventoAcademico
+    {
+
+        public int Id { get; set; }
+
+        [Display (Name= "Evento Academico")]
+        [MaxLength (80, ErrorMessage = "El campo {0} debe contener solo 80 caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string? Name { get; set; }
+
+
+
+    }
+}
